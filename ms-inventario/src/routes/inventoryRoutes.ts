7e6523bbe4f4990b5a3cd3fa3c8a6bda32360b06
AgusDev <agusalanis.dev@gmail.com>
2024-10-30 17:14:07 -0300

    añadiendo verificaciones

--- ms-inventario/src/routes/inventoryRoutes.ts
+++ ms-inventario/src/routes/inventoryRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { updateStock, getStockByProductId } from '../controllers/stockController';
+import { updateStock, getStockByProductId, checkInventory } from '../controllers/stockController';
 
 const router = Router();
 router.get('/stock/:producto_id', getStockByProductId);
 router.put('/stock/:producto_id', updateStock);
+router.get('/stock/check/:producto_id', checkInventory);
 
 export default router;
