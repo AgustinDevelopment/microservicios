0298efaa7e1144e6f32578e976420ca3acf717c2
AgusDev <156959270+AgustinDevelopment@users.noreply.github.com>
2024-11-27 23:16:52 -0300

    crear inventario

--- ms-inventario/src/routes/inventoryRoutes.ts
+++ ms-inventario/src/routes/inventoryRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { updateStock, getStockByProductId, checkInventory } from '../controllers/stockController';
+import { updateStock, getStockByProductId, checkInventory, createInventory } from '../controllers/stockController';
 
 const router = Router();
 router.get('/stock/:producto_id', getStockByProductId);
 router.put('/stock/:producto_id', updateStock);
 router.get('/stock/check/:producto_id', checkInventory);
+router.post('/stock/create', createInventory);
 
 export default router;
